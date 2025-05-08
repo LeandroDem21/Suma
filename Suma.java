37b3719bac3bd7e1fd471aac0ef0c029fb999b56
Leandro <20243rd051@utez.edu.mx>
2025-05-08 12:35:40 -0600

    Este codigo suma 3 numeros

---
+++ Suma.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Suma{
+public static void main(String[]args){
+Scanner almacenar = new Scanner(System.in);
+System.out.println("Escribe el 1er numero");
+int numero = almacenar.nextInt();
+System.out.println("Escribe el 2do numero");
+int numero2 = almacenar.nextInt();
+System.out.println("Escribe 3er numero");
+int numero3 = almacenar.nextInt();
+System.out.println("L suma de los numeros es:" + numero + " " + numero2 + " " + numero3 +);
+}
+
+}
